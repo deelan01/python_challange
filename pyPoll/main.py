b644c96e21878dd42e22c9b7a2e13128fcc4286e
deelan101 <deelanp93@GMAIL.COM>
2024-09-17 17:40:40 -0400

    final changes

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import numpy as np
 
+data = ("resources/election_data.csv")
+#convert to dataframe
+df = pd.read_csv(data)
+
+ballots_total = len(df)
+print(f"Ballots:",ballots_total)
+
+info = (df['Candidate'].value_counts()/df['Candidate'].count())*100
+df.loc['Canidate'] = df.sum()
+print(info)
--- pybank/main.py
+++ pybank/main.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import numpy as np
 
+#imported data
+data = ("resources/budget_data.csv")
+
+#convert to dataframe
+df = pd.read_csv(data)
+
+#print length of dates
+dates_length = len(df.Date)
+print(f"dates:", dates_length)
+
+#print sum of profits
+changes = df.sum()
+print(f"added total:", changes)
+
+#convert to nummeric and find average
+df['Profit/Losses'] = pd.to_numeric(df['Profit/Losses'])
+average = df['Profit/Losses'].diff()
+print(f"rounded difference:", {round(average.mean(),2)})
+
+#print max of data column rounded
+print(f"maximum:",{round(average.max(),0)})
+
+#print min of data column rounded
+print(f"miniumum:",{round(average.min(),0)})
